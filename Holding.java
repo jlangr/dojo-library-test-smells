32a747f37a6c9cb50253cfe01a0ff9dae6e5fb5e
Jeff Langr <jeff.langr@outpace.com>
2015-12-01 12:04:02 -0700

    retab

--- Holding.java
+++ Holding.java
@@ -1,139 +1,139 @@
 import java.util.Date;
 
 public class Holding {
-   public static final String BARCODE_SEPARATOR = ":";
-   private MaterialDetails material;
-   private Branch branch;
-   private Date dateCheckedOut;
-   private Date dateLastCheckedIn;
-   private int copyNumber;
+    public static final String BARCODE_SEPARATOR = ":";
+    private MaterialDetails material;
+    private Branch branch;
+    private Date dateCheckedOut;
+    private Date dateLastCheckedIn;
+    private int copyNumber;
 
-   public Holding(MaterialDetails material) {
-      this(material, Branch.CHECKED_OUT);
-   }
+    public Holding(MaterialDetails material) {
+        this(material, Branch.CHECKED_OUT);
+    }
 
-   public Holding(MaterialDetails material, Branch branch) {
-      this(material, branch, 1);
-   }
+    public Holding(MaterialDetails material, Branch branch) {
+        this(material, branch, 1);
+    }
 
-   public Holding(MaterialDetails material, Branch branch, int copyNumber) {
-      this.material = material;
-      this.branch = branch;
-      this.copyNumber = copyNumber;
-   }
+    public Holding(MaterialDetails material, Branch branch, int copyNumber) {
+        this.material = material;
+        this.branch = branch;
+        this.copyNumber = copyNumber;
+    }
 
-   public MaterialDetails getMaterial() {
-      return material;
-   }
+    public MaterialDetails getMaterial() {
+        return material;
+    }
 
-   public Branch getBranch() {
-      return branch;
-   }
+    public Branch getBranch() {
+        return branch;
+    }
 
-   public int getCopyNumber() {
-      return copyNumber;
-   }
+    public int getCopyNumber() {
+        return copyNumber;
+    }
 
-   public void transfer(Branch newBranch) {
-      branch = newBranch;
-   }
+    public void transfer(Branch newBranch) {
+        branch = newBranch;
+    }
 
-   public boolean isAvailable() {
-      return branch != Branch.CHECKED_OUT;
-   }
+    public boolean isAvailable() {
+        return branch != Branch.CHECKED_OUT;
+    }
 
-   public void checkOut(Date date) {
-      branch = Branch.CHECKED_OUT;
-      dateCheckedOut = date;
-   }
+    public void checkOut(Date date) {
+        branch = Branch.CHECKED_OUT;
+        dateCheckedOut = date;
+    }
 
-   public Date dateCheckedOut() {
-      return dateCheckedOut;
-   }
+    public Date dateCheckedOut() {
+        return dateCheckedOut;
+    }
 
-   public Date dateDue() {
-      return DateUtil.addDays(dateCheckedOut, getHoldingPeriod());
-   }
+    public Date dateDue() {
+        return DateUtil.addDays(dateCheckedOut, getHoldingPeriod());
+    }
 
-   private int getHoldingPeriod() {
-      return material.getFormat().getCheckoutPeriod();
-   }
+    private int getHoldingPeriod() {
+        return material.getFormat().getCheckoutPeriod();
+    }
 
-   public int checkIn(Date date, Branch branch) {
-      this.dateLastCheckedIn = date;
-      this.branch = branch;
-      int daysLate = DateUtil.daysFrom(dateDue(), dateLastCheckedIn);
-      return daysLate < 0 ? 0 : daysLate;
-   }
+    public int checkIn(Date date, Branch branch) {
+        this.dateLastCheckedIn = date;
+        this.branch = branch;
+        int daysLate = DateUtil.daysFrom(dateDue(), dateLastCheckedIn);
+        return daysLate < 0 ? 0 : daysLate;
+    }
 
-   public Date dateLastCheckedIn() {
-      return dateLastCheckedIn;
-   }
+    public Date dateLastCheckedIn() {
+        return dateLastCheckedIn;
+    }
 
-   public String getBarCode() {
-      return createBarCode(material.getClassification(), copyNumber);
-   }
+    public String getBarCode() {
+        return createBarCode(material.getClassification(), copyNumber);
+    }
 
-   public void setCopyNumber(int copyNumber) {
-      this.copyNumber = copyNumber;
-   }
+    public void setCopyNumber(int copyNumber) {
+        this.copyNumber = copyNumber;
+    }
 
-   public static String createBarCode(String classification, int copyNumber) {
-      return classification + BARCODE_SEPARATOR + copyNumber;
-   }
+    public static String createBarCode(String classification, int copyNumber) {
+        return classification + BARCODE_SEPARATOR + copyNumber;
+    }
 
-   @Override
-   public boolean equals(Object object) {
-      if (object == null)
-         return false;
-      if (this.getClass() != object.getClass())
-         return false;
-      Holding that = (Holding)object;
-      return this.getBarCode().equals(that.getBarCode());
-   }
+    @Override
+    public boolean equals(Object object) {
+        if (object == null)
+            return false;
+        if (this.getClass() != object.getClass())
+            return false;
+        Holding that = (Holding)object;
+        return this.getBarCode().equals(that.getBarCode());
+    }
 
-   @Override
-   public String toString() {
-      return material.toString() + "(" + copyNumber + ") @ " + branch.getName();
-   }
+    @Override
+    public String toString() {
+        return material.toString() + "(" + copyNumber + ") @ " + branch.getName();
+    }
 
-   @Override
-   public int hashCode() {
-      return getBarCode().hashCode();
-   }
+    @Override
+    public int hashCode() {
+        return getBarCode().hashCode();
+    }
 
-   public static int getCopyNumber(String barcode) {
-      String copy = splitOnColon(barcode)[1];
-      return parsePositiveInt(copy);
-   }
+    public static int getCopyNumber(String barcode) {
+        String copy = splitOnColon(barcode)[1];
+        return parsePositiveInt(copy);
+    }
 
-   public static String getClassification(String barcode) {
-      return splitOnColon(barcode)[0];
-   }
+    public static String getClassification(String barcode) {
+        return splitOnColon(barcode)[0];
+    }
 
-   private static int parsePositiveInt(String text) {
-      int number = parseInt(text);
-      if (number < 1)
-         throw new IllegalArgumentException();
-      return number;
-   }
+    private static int parsePositiveInt(String text) {
+        int number = parseInt(text);
+        if (number < 1)
+            throw new IllegalArgumentException();
+        return number;
+    }
 
-   private static String[] splitOnColon(String barcode) {
-      String[] barcodeParts = barcode.split(":");
-      if (barcodeParts.length != 2)
-         throw new IllegalArgumentException();
-      return barcodeParts;
-   }
+    private static String[] splitOnColon(String barcode) {
+        String[] barcodeParts = barcode.split(":");
+        if (barcodeParts.length != 2)
+            throw new IllegalArgumentException();
+        return barcodeParts;
+    }
 
-   private static int parseInt(String text) {
-      try {
-         return Integer.parseInt(text, 10);
-      } catch (Throwable t) {
-         throw new IllegalArgumentException();
-      }
-   }
+    private static int parseInt(String text) {
+        try {
+            return Integer.parseInt(text, 10);
+        } catch (Throwable t) {
+            throw new IllegalArgumentException();
+        }
+    }
 
-   public int daysLate() {
-      return DateUtil.daysAfter(dateDue(), dateLastCheckedIn());
-   }
+    public int daysLate() {
+        return DateUtil.daysAfter(dateDue(), dateLastCheckedIn());
+    }
 }
--- HoldingTest.java
+++ HoldingTest.java
@@ -5,189 +5,189 @@ import java.util.*;
 import org.junit.*;
 
 public class HoldingTest {
-   private Holding holding;
-   private static final Date TODAY = new Date();
-   private static final int COPY_NUMBER_1 = 1;
-   private static final Branch EAST_BRANCH = new Branch("East");
+    private Holding holding;
+    private static final Date TODAY = new Date();
+    private static final int COPY_NUMBER_1 = 1;
+    private static final Branch EAST_BRANCH = new Branch("East");
 
-   @Before
-   public void setUp() {
-      holding = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_EAST, COPY_NUMBER_1);
-   }
+    @Before
+    public void setUp() {
+        holding = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_EAST, COPY_NUMBER_1);
+    }
 
-   @Test
-   public void create() {
-      assertMaterial(Data.THE_TRIAL, holding);
-      assertEquals(BranchTest.BRANCH_EAST, holding.getBranch());
-      assertEquals(1, holding.getCopyNumber());
-   }
+    @Test
+    public void create() {
+        assertMaterial(Data.THE_TRIAL, holding);
+        assertEquals(BranchTest.BRANCH_EAST, holding.getBranch());
+        assertEquals(1, holding.getCopyNumber());
+    }
 
-   @Test
-   public void createWithBranchAndCopyDefaults() {
-      Holding holding = new Holding(Data.THE_TRIAL);
-      assertEquals(Branch.CHECKED_OUT, holding.getBranch());
-      assertEquals(1, holding.getCopyNumber());
-   }
+    @Test
+    public void createWithBranchAndCopyDefaults() {
+        Holding holding = new Holding(Data.THE_TRIAL);
+        assertEquals(Branch.CHECKED_OUT, holding.getBranch());
+        assertEquals(1, holding.getCopyNumber());
+    }
 
-   @Test
-   public void createWithCopyDefaults() {
-      Holding holding = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_EAST);
-      assertEquals(1, holding.getCopyNumber());
-   }
+    @Test
+    public void createWithCopyDefaults() {
+        Holding holding = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_EAST);
+        assertEquals(1, holding.getCopyNumber());
+    }
 
-   @Test
-   public void generatesBarCodeFromClassificationAndCopy() {
-      assertEquals(Data.THE_TRIAL.getClassification() + Holding.BARCODE_SEPARATOR
-            + COPY_NUMBER_1, holding.getBarCode());
+    @Test
+    public void generatesBarCodeFromClassificationAndCopy() {
+        assertEquals(Data.THE_TRIAL.getClassification() + Holding.BARCODE_SEPARATOR
+                + COPY_NUMBER_1, holding.getBarCode());
 
-      assertEquals("a" + Holding.BARCODE_SEPARATOR + "2",
-            Holding.createBarCode("a", 2));
-   }
+        assertEquals("a" + Holding.BARCODE_SEPARATOR + "2",
+                Holding.createBarCode("a", 2));
+    }
 
-   @Test
-   public void extractsClassificationFromBarcode() {
-      assertThat(Holding.getClassification("123:1"), is("123"));
-      assertThat(Holding.getClassification("456:1"), is("456"));
-   }
+    @Test
+    public void extractsClassificationFromBarcode() {
+        assertThat(Holding.getClassification("123:1"), is("123"));
+        assertThat(Holding.getClassification("456:1"), is("456"));
+    }
 
-   @Test
-   public void extractsCopyNumberFromBarcode() {
-      assertThat(Holding.getCopyNumber("123:1"), is(1));
-      assertThat(Holding.getCopyNumber("123:2"), is(2));
-   }
+    @Test
+    public void extractsCopyNumberFromBarcode() {
+        assertThat(Holding.getCopyNumber("123:1"), is(1));
+        assertThat(Holding.getCopyNumber("123:2"), is(2));
+    }
 
-   @Test(expected = IllegalArgumentException.class)
-   public void throwsWhenExtractingClassificationFromInvalidBarcode() {
-      Holding.getClassification("1234");
-   }
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsWhenExtractingClassificationFromInvalidBarcode() {
+        Holding.getClassification("1234");
+    }
 
-   @Test(expected = IllegalArgumentException.class)
-   public void throwsWhenExtractingCopyNumberFromInvalidBarcode() {
-      Holding.getCopyNumber("1234");
-   }
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsWhenExtractingCopyNumberFromInvalidBarcode() {
+        Holding.getCopyNumber("1234");
+    }
 
-   @Test(expected = IllegalArgumentException.class)
-   public void throwsWhenExtractingCopyNumberFromBarcodeWithNonNumericCopyNumber() {
-      Holding.getCopyNumber("1234:x");
-   }
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsWhenExtractingCopyNumberFromBarcodeWithNonNumericCopyNumber() {
+        Holding.getCopyNumber("1234:x");
+    }
 
-   @Test(expected = IllegalArgumentException.class)
-   public void throwsWhenExtractingNonPositiveCopyNumberFromBarcode() {
-      Holding.getCopyNumber("1234:0");
-   }
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsWhenExtractingNonPositiveCopyNumberFromBarcode() {
+        Holding.getCopyNumber("1234:0");
+    }
 
-   @Test
-   public void changesBranchOnTransfer() {
-      holding.transfer(BranchTest.BRANCH_WEST);
-      assertEquals(BranchTest.BRANCH_WEST, holding.getBranch());
-   }
+    @Test
+    public void changesBranchOnTransfer() {
+        holding.transfer(BranchTest.BRANCH_WEST);
+        assertEquals(BranchTest.BRANCH_WEST, holding.getBranch());
+    }
 
-   @Test
-   public void ck() {
-      holding.checkOut(TODAY);
-      assertEquals(TODAY, holding.dateCheckedOut());
-      assertTrue(holding.dateDue().after(TODAY));
-      assertEquals(Branch.CHECKED_OUT, holding.getBranch());
-      assertFalse(holding.isAvailable());
+    @Test
+    public void ck() {
+        holding.checkOut(TODAY);
+        assertEquals(TODAY, holding.dateCheckedOut());
+        assertTrue(holding.dateDue().after(TODAY));
+        assertEquals(Branch.CHECKED_OUT, holding.getBranch());
+        assertFalse(holding.isAvailable());
 
-      holding.checkOut(TODAY);
-      Date tomorrow = new Date(TODAY.getTime() + 60L + 60 * 1000 * 24);
-      holding.checkIn(tomorrow, EAST_BRANCH);
-      assertEquals(tomorrow, holding.dateLastCheckedIn());
-      assertTrue(holding.isAvailable());
-      assertEquals(EAST_BRANCH, holding.getBranch());
-   }
+        holding.checkOut(TODAY);
+        Date tomorrow = new Date(TODAY.getTime() + 60L + 60 * 1000 * 24);
+        holding.checkIn(tomorrow, EAST_BRANCH);
+        assertEquals(tomorrow, holding.dateLastCheckedIn());
+        assertTrue(holding.isAvailable());
+        assertEquals(EAST_BRANCH, holding.getBranch());
+    }
 
-   @Test
-   public void returnDateForStandardBook() {
-      holding.checkOut(TODAY);
-      Date dateDue = holding.dateDue();
-      assertDateEquals(addDays(TODAY, MaterialType.Book.getCheckoutPeriod()), dateDue);
-   }
+    @Test
+    public void returnDateForStandardBook() {
+        holding.checkOut(TODAY);
+        Date dateDue = holding.dateDue();
+        assertDateEquals(addDays(TODAY, MaterialType.Book.getCheckoutPeriod()), dateDue);
+    }
 
-   @Test
-   public void testSomething() {
-      // movie
-      checkOutToday(Data.DR_STRANGELOVE, BranchTest.BRANCH_EAST);
-      Date expected = addDays(TODAY, MaterialType.DVD.getCheckoutPeriod());
-      assertDateEquals(addDays(TODAY, MaterialType.DVD.getCheckoutPeriod()), holding.dateDue());
+    @Test
+    public void testSomething() {
+        // movie
+        checkOutToday(Data.DR_STRANGELOVE, BranchTest.BRANCH_EAST);
+        Date expected = addDays(TODAY, MaterialType.DVD.getCheckoutPeriod());
+        assertDateEquals(addDays(TODAY, MaterialType.DVD.getCheckoutPeriod()), holding.dateDue());
 
-      // childrens movie
-      checkOutToday(Data.THE_TRIAL_NEW_EDITION, BranchTest.BRANCH_EAST);
-      expected = addDays(TODAY, MaterialType.Book.getCheckoutPeriod());
-      assertDateEquals(expected, holding.dateDue());
-   }
+        // childrens movie
+        checkOutToday(Data.THE_TRIAL_NEW_EDITION, BranchTest.BRANCH_EAST);
+        expected = addDays(TODAY, MaterialType.Book.getCheckoutPeriod());
+        assertDateEquals(expected, holding.dateDue());
+    }
 
-   @Test
-   public void answersDaysLateOfZeroWhenReturnedSameDay() {
-      checkOutToday(Data.THE_TRIAL, BranchTest.BRANCH_EAST);
+    @Test
+    public void answersDaysLateOfZeroWhenReturnedSameDay() {
+        checkOutToday(Data.THE_TRIAL, BranchTest.BRANCH_EAST);
 
-      int daysLate = holding.checkIn(TODAY, BranchTest.BRANCH_EAST);
+        int daysLate = holding.checkIn(TODAY, BranchTest.BRANCH_EAST);
 
-      assertThat(daysLate, is(0));
-   }
+        assertThat(daysLate, is(0));
+    }
 
-   @Test
-   public void answersDaysLateOfZeroWhenReturnedOnDateDue() {
-      checkOutToday(Data.THE_TRIAL, BranchTest.BRANCH_EAST);
+    @Test
+    public void answersDaysLateOfZeroWhenReturnedOnDateDue() {
+        checkOutToday(Data.THE_TRIAL, BranchTest.BRANCH_EAST);
 
-      int daysLate = holding.checkIn(holding.dateDue(), BranchTest.BRANCH_EAST);
+        int daysLate = holding.checkIn(holding.dateDue(), BranchTest.BRANCH_EAST);
 
-      assertThat(daysLate, is(0));
-   }
+        assertThat(daysLate, is(0));
+    }
 
-   @Test
-   public void answersDaysLateWhenReturnedAfterDueDate() {
-      checkOutToday(Data.THE_TRIAL, BranchTest.BRANCH_EAST);
-      Date threeDaysLate = DateUtil.addDays(holding.dateDue(), 3);
+    @Test
+    public void answersDaysLateWhenReturnedAfterDueDate() {
+        checkOutToday(Data.THE_TRIAL, BranchTest.BRANCH_EAST);
+        Date threeDaysLate = DateUtil.addDays(holding.dateDue(), 3);
 
-      int daysLate = holding.checkIn(threeDaysLate, BranchTest.BRANCH_EAST);
+        int daysLate = holding.checkIn(threeDaysLate, BranchTest.BRANCH_EAST);
 
-      assertThat(daysLate, is(3));
-   }
+        assertThat(daysLate, is(3));
+    }
 
-   private void checkOutToday(MaterialDetails material, Branch branch) {
-      holding = new Holding(material, branch);
-      holding.checkOut(TODAY);
-   }
+    private void checkOutToday(MaterialDetails material, Branch branch) {
+        holding = new Holding(material, branch);
+        holding.checkOut(TODAY);
+    }
 
-   static void assertContains(List<Holding> holdings, MaterialDetails expectedMaterial) {
-      assertMaterial(expectedMaterial, holdings.get(0));
-   }
+    static void assertContains(List<Holding> holdings, MaterialDetails expectedMaterial) {
+        assertMaterial(expectedMaterial, holdings.get(0));
+    }
 
-   static void assertMaterial(MaterialDetails expected, Holding holding) {
-      MaterialDetails actual = holding.getMaterial();
-      assertEquals(expected.getAuthor(), actual.getAuthor());
-      assertEquals(expected.getClassification(), actual.getClassification());
-      assertEquals(expected.getTitle(), actual.getTitle());
-      assertEquals(expected.getYear(), actual.getYear());
-   }
+    static void assertMaterial(MaterialDetails expected, Holding holding) {
+        MaterialDetails actual = holding.getMaterial();
+        assertEquals(expected.getAuthor(), actual.getAuthor());
+        assertEquals(expected.getClassification(), actual.getClassification());
+        assertEquals(expected.getTitle(), actual.getTitle());
+        assertEquals(expected.getYear(), actual.getYear());
+    }
 
-   public static Date addDays(Date date, int days) {
-      return new Date(date.getTime() + days * 60L * 1000 * 60 * 24);
-   }
+    public static Date addDays(Date date, int days) {
+        return new Date(date.getTime() + days * 60L * 1000 * 60 * 24);
+    }
 
-   public static void assertDateEquals(Date expectedDate, Date actualDate) {
-      Calendar calendar = Calendar.getInstance();
-      calendar.setTime(expectedDate);
-      int expectedYear = calendar.get(Calendar.YEAR);
-      int expectedDayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
-      calendar.setTime(actualDate);
-      assertEquals(expectedYear, calendar.get(Calendar.YEAR));
-      assertEquals(expectedDayOfYear, calendar.get(Calendar.DAY_OF_YEAR));
-   }
+    public static void assertDateEquals(Date expectedDate, Date actualDate) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(expectedDate);
+        int expectedYear = calendar.get(Calendar.YEAR);
+        int expectedDayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
+        calendar.setTime(actualDate);
+        assertEquals(expectedYear, calendar.get(Calendar.YEAR));
+        assertEquals(expectedDayOfYear, calendar.get(Calendar.DAY_OF_YEAR));
+    }
 
-   @Test
-   public void equality() {
-      Holding holding1 = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_EAST, 1);
-      Holding holding1Copy1 = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_WEST, 1); // diff loc but same copy
-      Holding holding1Copy2 = new Holding(Data.THE_TRIAL, Branch.CHECKED_OUT, 1);
-      Holding holding2 = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_EAST, 2); // 2nd copy
-      Holding holding1Subtype = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_EAST,
-            1) {
-      };
+    @Test
+    public void equality() {
+        Holding holding1 = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_EAST, 1);
+        Holding holding1Copy1 = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_WEST, 1); // diff loc but same copy
+        Holding holding1Copy2 = new Holding(Data.THE_TRIAL, Branch.CHECKED_OUT, 1);
+        Holding holding2 = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_EAST, 2); // 2nd copy
+        Holding holding1Subtype = new Holding(Data.THE_TRIAL, BranchTest.BRANCH_EAST,
+                1) {
+        };
 
-      new EqualityTester(holding1, holding1Copy1, holding1Copy2, holding2,
-            holding1Subtype).verify();
-   }
+        new EqualityTester(holding1, holding1Copy1, holding1Copy2, holding2,
+                holding1Subtype).verify();
+    }
 }
